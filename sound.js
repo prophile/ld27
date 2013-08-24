918122d74a23e9db694859dad6c23d08d69fe8e5
Alastair Lynn <arplynn@gmail.com>
2013-08-24 19:30:50 +0100

    Fix the music unpredictability

--- sound.js
+++ sound.js
@@ -1,17 +1,26 @@
 var Sound = (function() {
     var MUSIC_SOURCE = "https://dl.dropboxusercontent.com/s/yxlhaq0abhdpxc3/music.mp3?token_hash=AAEHavddSaEmHL6s2nm3kVNdeVzTmTWHZB2cC9sQkQ24mQ&dl=1";
-    var music = new Howl({urls: [MUSIC_SOURCE], buffer: true});
 
     var musicRequested = new Bacon.Bus();
     var musicEnabled = new Bacon.Bus();
+    var musicLoaded = new Bacon.Bus();
 
     Constants.get('music_enable', function(enabled) {
         musicEnabled.push(enabled);
     });
 
-    var musicPlaying = musicRequested.toProperty(false).and(musicEnabled.toProperty(true));
+    var music = new Howl({urls: [MUSIC_SOURCE],
+                          buffer: true,
+                          onload: function() {
+                              musicLoaded.push(true);
+                              console.log("Finished loading music.");
+                          }});
 
-    var musicStartStop = musicPlaying.changes().skipDuplicates();
+    var musicPlaying = (musicRequested.toProperty(false)
+                                      .and(musicEnabled.toProperty(true))
+                                      .and(musicLoaded.toProperty(false)));
+
+    var musicStartStop = musicPlaying.changes().skip(1).skipDuplicates();
 
     musicStartStop.filter(function(x) { return x; }).onValue(function() {
         music.play();
