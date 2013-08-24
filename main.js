eb5ed57f4bfdf6c1e002963b4ec2ef7b298e85b8
Alastair Lynn <arplynn@gmail.com>
2013-08-24 13:30:48 +0100

    Add embarrassing oversights

---
+++ main.js
@@ -0,0 +1,30 @@
+$(function() {
+    var stage = new PIXI.Stage(0x66FF99);
+    var renderer = PIXI.autoDetectRenderer(400, 400);
+
+    var texture, object;
+
+    var animate = function() {
+        requestAnimFrame(animate);
+
+        object.rotation += 0.1
+
+        renderer.render(stage);
+    };
+
+    document.body.appendChild(renderer.view);
+
+    requestAnimFrame(animate);
+
+    texture = PIXI.Texture.fromImage('bee.png');
+    object = new PIXI.Sprite(texture);
+
+    object.anchor.x = 0.5;
+    object.anchor.y = 0.5;
+
+    object.position.x = 200;
+    object.position.y = 150;
+
+    stage.addChild(object);
+});
+
